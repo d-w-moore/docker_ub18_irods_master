44c1a75852cdd64096910c99ad70232ba07690b6
d-w-moore <dmoore@renci.org>
2019-09-18 06:51:12 -0400

    better script and words, '*' option

--- ubuntu18_irods_setup.sh
+++ ubuntu18_irods_setup.sh
@@ -2,49 +2,56 @@
 
 choice="$1"
 
+LOCAL_REPO=~/github
+INSTALL_SCRIPT_DIR=ubuntu_irods_installer
+INSTALL_SCRIPT="$LOCAL_REPO/$INSTALL_SCRIPT_DIR/install.sh"
+
 MENU=(
-  build_dir_setup
+  setup_build_dirs
   install_irods_prereqs
-  compile_irods
+  build_irods_packages
 )
 
 if [ ! "$choice" ] ; then
   echo  -n "OPTIONS : ${MENU[*]} -> "
   read choice
+elif [ "$choice" = '*' ]; then
+  choice=" ${MENU[*]} "
 fi
 
-if [ "$choice" = build_dir_setup ]; then
+if [[ "$choice" = *\ setup_build_dirs\ * ]]; then
 
-  mkdir ~/github  &&  cd ~/github && \
+  mkdir "$LOCAL_REPO" && cd "$LOCAL_REPO" && \
   git clone http://github.com/irods/irods && \
   git clone http://github.com/irods/irods_client_icommands
-  git clone http://bitbucket.org/dan65536/rodcycle
-  cd ~/github/irods && git submodule update --init ; cd ~/github
+  git clone http://github.com/d-w-moore/"$INSTALL_SCRIPT_DIR"
+
+  cd "$LOCAL_REPO"/irods && git submodule update --init ; cd "$LOCAL_REPO"
   for x in irods*/ ; do mkdir build__$x;done
 
-elif [ "$choice" = install_irods_prereqs ]; then
+elif [[ "$choice" = *\ install_irods_prereqs\ * ]]; then
 
-  ~/github/rodcycle/reinstall.sh -C --i=4.3.0 --w='config-essentials add-build-externals create-db'
+  "$INSTALL_SCRIPT" -C --i=4.3.0 --w='config-essentials add-build-externals create-db'
 
-elif [ "$choice" = compile_irods ]; then
+elif [ "$choice" = *\ build_irods_packages\ * ]; then
 
   PATH=/opt/irods-externals/cmake3.11.4-0/bin:$PATH
 
-  cd ~/github/build__irods && \
+  cd "$LOCAL_REPO"/build__irods && \
   echo -e '*********************************\n** building irods server and runtime **\n******************************' && \
   cmake -GNinja ../irods -DCMAKE_BUILD_TYPE=Debug && \
   ninja package && \
-  ~/github/rodcycle/reinstall.sh -C --i=4.3.0 --w=basic 4
+   "$INSTALL_SCRIPT" -C --i=4.3.0 --w=basic 4
 
-  [ $? -eq 0 ] || { echo >&2 "failed to compile."; exit 1; }
+  [ $? -eq 0 ] || { echo >&2 "Failed to build iRODS main packages."; exit 1; }
 
-  cd ~/github/build__irods_client_icommands && \
+  cd "$LOCAL_REPO"/build__irods_client_icommands && \
   echo -e '************************\n** building icommands **\n************************' && \
   cmake -GNinja ../irods_client_icommands -DCMAKE_BUILD_TYPE=Debug && \
   ninja package && \
-  ~/github/rodcycle/reinstall.sh -C --i=4.3.0 4 5
+  "$INSTALL_SCRIPT" -C --i=4.3.0 4 5
 
-  [ $? -eq 0 ] || { echo >&2 "failed to compile."; exit 1; }
+  [ $? -eq 0 ] || { echo >&2 "failed to iRODS icommands packages."; exit 1; }
 
 else
 
